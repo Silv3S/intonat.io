760749621a3f17dbc8758508b3cd454c0c174b49
Silv3S <slawomir.s.siwek@gmail.com>
2021-05-01 20:57:27 +0200

    Hello world

---
+++ pbaudio/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pbaudio project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pbaudio.settings')
+
+application = get_asgi_application()
